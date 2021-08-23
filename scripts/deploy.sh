6914bfcb54f3d8b77fcf9470b87bb8de982c3cab
Martin Maillard <git@martinmaillard.com>
2021-08-23 09:53:14 +0200

    chore: add deployment script

---
+++ scripts/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$(git status --untracked-files=no --porcelain)" ]; then
+    echo 'Starting deployment...\n'
+else
+    echo 'Repository is not clean. Aborting.\n'
+    exit 1
+fi
+
+git checkout --orphan gh-pages
+npm run build
+echo "sketch.zazuko.com" > dist/CNAME
+git --work-tree dist add --all
+git --work-tree dist commit -m 'Deploy'
+git push origin HEAD:gh-pages --force
+rm -r dist
+git checkout -f master
+git branch -D gh-pages
