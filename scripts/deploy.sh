a980a5a379c3a2bcbcd31efc23bcac64f911a020
Martin Maillard <git@martinmaillard.com>
2021-08-23 10:01:42 +0200

    chore: fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,5 +16,5 @@ git --work-tree dist add --all
 git --work-tree dist commit -m 'Deploy'
 git push origin HEAD:gh-pages --force
 rm -r dist
-git checkout -f master
+git checkout -f main
 git branch -D gh-pages
