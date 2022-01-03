5cfc8516f5e6dd86823b5b50924d692ad9318fc2
Martin Maillard <git@martinmaillard.com>
2022-01-03 15:37:15 +0100

    chore: upgrade tailwind to 3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['public/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['public/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
@@ -22,9 +20,6 @@ module.exports = {
         title: ['Playfair Display', 'serif'],
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 }
