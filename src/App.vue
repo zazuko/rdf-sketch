523d47b419dd086691f5aaaefbd459a028ba0fb0
Hofstetter Benjamin (extern) <benjamin.hofstetter@mobi.ch>
2023-01-21 15:50:47 +0100

    fix font

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,6 @@ export default defineComponent({
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
