617e4b4ce7e092ac94f55525931d95007868a697
BenjaminHofstetter <benjamin.hofstetter@me.com>
2025-02-07 15:30:04 +0000

    Deploying to gh-pages from @ zazuko/rdf-sketch@562ebacccfe6b77f7e6eee9459d2474a1e73e6e0 🚀

---
+++ assets/CustomJsonLdSerializer-6Yly00Iu.js
@@ -0,0 +1,1 @@
+import{S as e}from"./main-_snonK7K.js";class t extends e{constructor({...r}={}){super({encoding:"string",...r})}}export{t as default};
--- assets/CustomJsonLdSerializer-CtsYTzGF.js
+++
@@ -1,1 +0,0 @@
-import{S as e}from"./main-VqHoimz8.js";class t extends e{constructor({...r}={}){super({encoding:"string",...r})}}export{t as default};
