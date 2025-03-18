4fe6248673dec584faf352c4ce2ba42d5b7ba258
Hofstetter Benjamin (extern) <benjamin.hofstetter@mobi.ch>
2025-03-18 20:28:41 +0100

    update dependencies

--- src-vscode/CHANGELOG.md
+++ src-vscode/CHANGELOG.md
@@ -1,5 +1,8 @@
 # Change Log
 
+## 1.0.6
+- Update dependencies
+
 ## 1.0.5
 - Improve SPO scroll performance
 - Unique rdf:type Icon
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { createApp } from 'vue';
 
 import PrimeVue from 'primevue/config';
 
-import Aura from '@primevue/themes/aura';
+import Aura from '@primeuix/themes/aura';
 import App from './App.vue';
 
 import { useUrlSearchParams } from '@vueuse/core';
